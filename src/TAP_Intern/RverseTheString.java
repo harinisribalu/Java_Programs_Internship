1a0eedbb2e1af507403b4d3d26168b406f83dff5
harinisri <harinisribalu@gmail.com>
2026-06-11 22:08:16 +0530

    reverse the string

---
+++ src/TAP_Intern/RverseTheString.java
@@ -0,0 +1,20 @@
+package TAP_Intern;
+
+import java.util.Scanner;
+
+public class RverseTheString {
+	public static void main(String[] args) {
+		Scanner sc=new  Scanner (System.in);
+		String s= sc.nextLine();
+		String[]arr= s.split(" ");
+		for(int i=0;i<arr.length;i++) {
+			String t="";
+			for(int j=arr[i].length()-1;j>=0;j--) {
+				t=t+arr[i].charAt(j);
+			}
+			System.out.print(t+" ");
+		}
+		
+	}
+
+}
